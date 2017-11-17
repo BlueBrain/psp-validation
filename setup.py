9cb65e6dd8d0dd7330a29e584f5bb6a6dc017d03
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2017-11-17 13:48:11 +0100

    Parsing `psp` arguments with 'click'
     * Introduced `psp run` sub-command
    
    Change-Id: Ice92537d7ab9043e68cc2c6b88bc899413179639

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ setup(
     name="psp_validation",
     version=__version__,
     install_requires=[
+        'click>=6.0',
         'numpy>=1.10',
         'h5py>=2.7',
-        'bglibpy>=3.2.2,<4.0',
-        'bluepy>=0.11,<0.12',
+        'bglibpy>=3.2.6,<4.0',
+        'bluepy>=0.11.10,<0.12',
         'nose>=1.3',
     ],
     packages=[
