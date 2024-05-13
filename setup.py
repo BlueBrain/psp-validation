0a23181e40287aa6d13486ba0ec87918fa4d8a8c
Mike Gevaert <michael.gevaert@epfl.ch>
2024-05-13 15:45:28 +0200

    initial OSS version

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
+"""setup"""
 from setuptools import find_packages, setup
-
-from psp_validation.version import __version__
 
 setup(
     name="psp-validation",
-    version=__version__,
     install_requires=[
         "attrs>=20.3.0",
         "click>=7.0",
@@ -26,25 +24,28 @@ setup(
     description="PSP analysis tools",
     long_description="PSP analysis tools",
     long_description_content_type="text/plain",
-    license="BBP-internal-confidential",
+    license="Apache2.0",
     python_requires=">=3.7",
-    entry_points={
-        "console_scripts": [
-            "psp=psp_validation.cli:cli",
-            "cv-validation=psp_validation.cv_validation.cli:cli",
-        ]
+    setup_requires=[
+        "setuptools_scm",
+    ],
+    use_scm_version={
+        "write_to": "psp_validation/version.py",
     },
-    url="https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
-    download_url="git@bbpgitlab.epfl.ch:nse/psp-validation.git",
+    entry_points={
+        "console_scripts": ["psp=psp_validation.cli:cli",
+                            "cv-validation=psp_validation.cv_validation.cli:cli"]},
+    url="https://github.com/BlueBrain/psp-validation/",
     project_urls={
-        "Tracker": "https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
-        "Source": "https://bbpgitlab.epfl.ch/nse/psp-validation",
+        "Tracker": "https://github.com/BlueBrain/psp-validation/issues",
+        "Source": "https://github.com/BlueBrain/psp-validation/",
     },
     include_package_data=True,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
